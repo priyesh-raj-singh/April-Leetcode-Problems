f5c1d1bcc0ec88969ec75e35c856f37defdba722
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-04-13 12:57:50 +0530

    Time: 3 ms (52.53%), Space: 5.8 MB (88.05%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<1)
+            return false;
+        for(int i = 2 ; i<6 ; i++){
+            while(n%i==0)
+                n = n/i;
+        }
+        if(n==1)
+            return true;
+        
+        return false;
+        
+    }
+};
