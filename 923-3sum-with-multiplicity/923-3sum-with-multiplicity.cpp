17c805d402abd8881a1f65fdaac41e6b6d0efa9b
Priyesh Raj Singh <65354440+priyesh-raj-singh@users.noreply.github.com>
2022-04-26 11:16:24 +0530

    Time: 1263 ms (21.74%), Space: 10.9 MB (17.73%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,76 +1,18 @@
 class Solution {
 public:
-      long long mod = 1e9 + 7;
-    
     int threeSumMulti(vector<int>& arr, int target) {
-        
         int n = arr.size();
-        
-        sort(arr.begin(), arr.end());
-        
-        int count = 0;
-        
-        for(int i = 0; i <= n - 3; i++)
-        {
-            int low = i + 1;
-            
-            int high = n - 1;
-            
-            int need = target - arr[i];
-            
-            while(low < high)
-            {
-                if(arr[low] + arr[high] < need)
-                {
-                    low++;
-                }
-                
-                else if(arr[low] + arr[high] > need)
-                {
-                    high--;
-                }
-                
-                else
-                {
-                    if(arr[low] != arr[high])
-                    {
-                        int left_count = 1;
-                        
-                        int right_count = 1;
-                        
-                        while(low < high && arr[low] == arr[low + 1])
-                        {
-                            left_count++;
-                            
-                            low++;
-                        }
-                        
-                        while(high > low && arr[high] == arr[high - 1])
-                        {
-                            right_count++;
-                            
-                            high--; 
-                        }
-                        
-                        count = (count % mod + left_count % mod * right_count % mod) % mod;
-                        
-                        low++;
-                        
-                        high--;
-                    }
-                    
-                    else
-                    {
-                        int total_duplicate = (high - low + 1);
-                        
-                        count = (count % mod + (total_duplicate % mod * (total_duplicate - 1) % mod) / 2) % mod;
-                        
-                        break;
-                    }
-                }
+        long long count = 0 ; 
+        unordered_map<int , int> mp;
+        for(int i = 0 ; i<n ; i++){
+            count+=mp[target - arr[i]];
+            count%=1000000007;
+            for(int j = 0 ; j<i ; j++){
+                mp[arr[i]+arr[j]]++;
             }
         }
-        
         return count;
+        
+        
     }
 };
